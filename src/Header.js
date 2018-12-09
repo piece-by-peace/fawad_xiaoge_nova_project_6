66bc01b79df435bac5c2dc198c4ce2d8da0f1199
900apm <xiaoge.deng@hotmail.com>
2018-12-09 03:03:06 -0500

    updated buttons

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ score, timer }) => {
     return (
-        <header>
+        <header className="clearfix">
             <div>Score: {score}</div>
             <div id="timer">Timer: {timer}</div>
         </header>
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const LandingPage = ({ handleOnClickButton }) => {
     return (
-        <div className="landing-page">
+        <div className="landing_page">
             <h1>Piece by peace</h1>
             <h2>Choose difficulty</h2>
             <div>
-                <button onClick={handleOnClickButton} value="easy">
+                <button className="button-stripe" onClick={handleOnClickButton} value="easy">
                     easy
                 </button>
 
-                <button onClick={handleOnClickButton} value="medium">
+                <button className="button-stripe" onClick={handleOnClickButton} value="medium">
                     medium
                 </button>
 
-                <button onClick={handleOnClickButton} value="hard">
+                <button className="button-stripe" onClick={handleOnClickButton} value="hard">
                     hard
                 </button>
             </div>

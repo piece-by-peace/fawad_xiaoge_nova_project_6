c0d87b6875ecb5186667c19ec89e8d9c93a754ce
900apm <xiaoge.deng@hotmail.com>
2018-12-09 04:56:55 -0500

    finished button styling

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,18 +3,18 @@ import React from 'react';
 const LandingPage = ({ handleOnClickButton }) => {
     return (
         <div className="landing_page">
-            <h1>Piece by peace</h1>
+            <h1>Piece by Peace</h1>
             <h2>Choose difficulty</h2>
             <div>
-                <button className="button-stripe" onClick={handleOnClickButton} value="easy">
+                <button onClick={handleOnClickButton} value="easy">
                     easy
                 </button>
 
-                <button className="button-stripe" onClick={handleOnClickButton} value="medium">
+                <button onClick={handleOnClickButton} value="medium">
                     medium
                 </button>
 
-                <button className="button-stripe" onClick={handleOnClickButton} value="hard">
+                <button onClick={handleOnClickButton} value="hard">
                     hard
                 </button>
             </div>

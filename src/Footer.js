6c33cc57d095474cf8447e061cbea595dcb97b6a
Fawad <fawad.a.qazi@gmail.com>
2018-12-11 11:23:57 -0500

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+
+   return (
+      <footer>
+         <p>
+            <a className="dev-name" href="http://fawadcodes.com" target="_blank">Fawad</a>
+            <a className="dev-name" href="https://novaquach.com/" target="_blank">Nova</a>
+            <a className="dev-name" href="http://xiaogedeng.com/" target="_blank">Xiaoge</a>
+            &copy; 2018
+         </p>
+      </footer>
+   );
+}
+
+export default Footer;
